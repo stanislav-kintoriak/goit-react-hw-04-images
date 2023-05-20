72cb3dca68021fb80f4f490bf8809bcebe754402
Stanislav Kintoriak <kintoriak@gmail.com>
2023-05-20 23:26:23 +0300

    Commit 3
    1) refactored code to hooks in the component Modal.js;

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,33 +1,82 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({modalClose,children}) => {
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+useEffect(()=>{
+  window.addEventListener('keydown', handleKeyDown)
 
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.modalClose();
-    }
-  };
-
-  handleBackdropClick = event => {
-    if ((event.currentTarget === event.target)) {
-      this.props.modalClose();
-    }
-  };
-
-  render() {
-    return (
-      <div onClick={this.handleBackdropClick} className={css.overlay}>
-        <div className={css.modal}>{this.props.children}</div>
-      </div>
-    );
-  }
+return () => {
+  window.removeEventListener('keydown', handleKeyDown)
 }
+},
+[handleKeyDown]
+)
+
+
+
+const  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+     modalClose();
+    }
+  };
+
+const  handleBackdropClick = event => {
+    if ((event.currentTarget === event.target)) {
+      modalClose();
+    }
+  };
+
+
+  return (
+          <div onClick={handleBackdropClick} className={css.overlay}>
+            <div className={css.modal}>{children}</div>
+          </div>
+        );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.modalClose();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if ((event.currentTarget === event.target)) {
+//       this.props.modalClose();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div onClick={this.handleBackdropClick} className={css.overlay}>
+//         <div className={css.modal}>{this.props.children}</div>
+//       </div>
+//     );
+//   }
+// }
